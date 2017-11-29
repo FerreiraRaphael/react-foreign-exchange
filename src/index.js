e4675ebc655984123dfa86e388cc99da196b0351
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-11-28 22:52:06 -0200

    Adding semantic ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
